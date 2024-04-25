55d61731c47e7d5e1b60dae42e06e70ade5d22b1
Piotr Fidler <fidler.piotr@gmail.com>
2024-04-25 13:00:26 +0200

    remove setTimeout to Glightbox function add DOOMcontentloaded

--- ktLightbox.js
+++ ktLightbox.js
@@ -1,21 +1,21 @@
 (function () {
-	if (window.location.pathname === '/komunikaty-turystyczne') {
-		var imgs = document.querySelectorAll('.information__content img');
-		imgs.forEach(function (img) {
-			debugger;
-			let url = img.src;
-			let cl = img.className;
-			let alt = img.alt;
-			console.log(alt);
-			var template = `<a href=${url} class="glightbox">
-<img class=${cl} src=${url} alt="${alt}" height="84">
-</a>`;
-			console.log(template);
-			img.parentElement.insertAdjacentHTML('beforeend', template);
-			img.remove();
-		});
-		setTimeout(() => {
+	document.addEventListener('DOMContentLoaded', (e) => {
+		if (window.location.pathname === '/komunikaty-turystyczne') {
+			var imgs = document.querySelectorAll('.information__content img');
+			imgs.forEach(function (img) {
+				debugger;
+				let url = img.src;
+				let cl = img.className;
+				let alt = img.alt;
+				console.log(alt);
+				var template = `<a href=${url} class="glightbox">
+	<img class=${cl} src=${url} alt="${alt}" height="84">
+	</a>`;
+				console.log(template);
+				img.parentElement.insertAdjacentHTML('beforeend', template);
+				img.remove();
+			});
 			GLightbox();
-		}, 1000);
-	}
+		}
+	});
 })();

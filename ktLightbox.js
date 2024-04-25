d5990a75313fd5218fc6b5b5585e8d26ee49b77a
Piotr Fidler <fidler.piotr@gmail.com>
2024-04-25 12:39:24 +0200

    add light box to komunikat turystyczny

---
+++ ktLightbox.js
@@ -0,0 +1,19 @@
+(function () {
+	if (window.location.pathname === '/komunikaty-turystyczne') {
+		var imgs = document.querySelectorAll('.information__content img');
+		imgs.forEach(function (img) {
+			debugger;
+			let url = img.src;
+			let cl = img.className;
+			let alt = img.alt;
+			console.log(alt);
+			var template = `<a href=${url} class="glightbox">
+<img class=${cl} src=${url} alt="${alt}" height="84">
+</a>`;
+			console.log(template);
+			img.parentElement.insertAdjacentHTML('beforeend', template);
+			img.remove();
+		});
+		GLightbox();
+	}
+})();

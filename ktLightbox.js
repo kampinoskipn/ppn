c1f4109a4c6e1967386e36982e8f9a919235c3ef
Piotr Fidler <fidler.piotr@gmail.com>
2024-04-26 11:54:53 +0200

    add condition for imgs

--- ktLightbox.js
+++ ktLightbox.js
@@ -1,15 +1,15 @@
 (function () {
 	document.addEventListener('DOMContentLoaded', (e) => {
-		var imgs = document.querySelectorAll('.information__content img');
+		let imgs = document.querySelectorAll('.information__content img');
 		if (
 			window.location.pathname === '/komunikaty-turystyczne' &&
 			imgs.length > 0
 		) {
 			imgs.forEach(function (img) {
 				debugger;
-				let url = img.src;
-				let cl = img.className;
-				let alt = img.alt;
+				const url = img.src;
+				const cl = img.className;
+				const alt = img.alt;
 				var template = `<a href=${url} class="glightbox">
 	<img class=${cl} src=${url} alt="${alt}" height="84">
 	</a>`;

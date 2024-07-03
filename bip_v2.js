84cc23527f4903314e0ca6a0d792953975bc1b87
Piotr Fidler <fidler.piotr@gmail.com>
2024-07-03 08:49:03 +0200

    exclude bip & sklep

--- bip_v2.js
+++ bip_v2.js
@@ -1,43 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-	(function () {
-		const whiteBIP = '/uploads/files/660d16e0cbcd9361647248.png';
-		const redBIP = '/uploads/files/660d0ae3d3e4a349043535.png';
-		const html = `<div class="menu__controls__button-container-bip">
-		<a href="https://bip.kampinoski-pn.gov.pl" aria-label="Biuletym informacji publicznej Kampinoskiego Parku Narodowego" target="_blank" rel=”noopener”>
-			<img src="${whiteBIP}" alt="BIP logo">
-			<img src="${redBIP}" alt="BIP logo">
-		</a></div>`;
+	if (
+		!window.location.pathname.includes('/bip') &&
+		!window.location.pathname.includes('/sklep')
+	) {
+		(function () {
+			const whiteBIP = '/uploads/files/660d16e0cbcd9361647248.png';
+			const redBIP = '/uploads/files/660d0ae3d3e4a349043535.png';
+			const html = `<div class="menu__controls__button-container-bip">
+				<a href="https://bip.kampinoski-pn.gov.pl" aria-label="Biuletym informacji publicznej Kampinoskiego Parku Narodowego" target="_blank" rel=”noopener”>
+					<img src="${whiteBIP}" alt="BIP logo">
+					<img src="${redBIP}" alt="BIP logo">
+				</a></div>`;
 
-		const getHeader = document.querySelector('header.menu');
-		const menuInner = document.querySelector('.menu__controls__inner');
-		if (getHeader != null) {
-			menuInner.insertAdjacentHTML('beforeend', html);
-		}
-
-		const getBip = document.querySelectorAll(
-			'.menu__controls__button-container-bip img',
-		);
-		if (getBip.length == 2) {
-			window.location.pathname == '/'
-				? (getBip[1].style.display = 'none')
-				: (getBip[0].style.display = 'none');
-		}
-
-		function changeImg(headerClass) {
-			if (window.location.pathname == '/' && headerClass == true) {
-				getBip[1].style.display = 'none';
-				getBip[0].style.display = 'block';
-			} else {
-				getBip[0].style.display = 'none';
-				getBip[1].style.display = 'block';
+			const getHeader = document.querySelector('header.menu');
+			const menuInner = document.querySelector('.menu__controls__inner');
+			if (getHeader != null) {
+				menuInner.insertAdjacentHTML('beforeend', html);
 			}
-		}
-		window.addEventListener('scroll', function () {
-			let headerClass =
-				getHeader.classList.contains('filter-transparent');
-			if (headerClass !== null) {
-				changeImg(headerClass);
+
+			const getBip = document.querySelectorAll(
+				'.menu__controls__button-container-bip img',
+			);
+			if (getBip.length == 2) {
+				window.location.pathname == '/'
+					? (getBip[1].style.display = 'none')
+					: (getBip[0].style.display = 'none');
 			}
-		});
-	})();
+
+			function changeImg(headerClass) {
+				if (window.location.pathname == '/' && headerClass == true) {
+					getBip[1].style.display = 'none';
+					getBip[0].style.display = 'block';
+				} else {
+					getBip[0].style.display = 'none';
+					getBip[1].style.display = 'block';
+				}
+			}
+			window.addEventListener('scroll', function () {
+				let headerClass =
+					getHeader.classList.contains('filter-transparent');
+				if (headerClass !== null) {
+					changeImg(headerClass);
+				}
+			});
+		})();
+	}
 });

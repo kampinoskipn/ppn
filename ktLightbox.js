c9249e643810598b2eee6ca50e2ca3cc32cea29a
Piotr Fidler <fidler.piotr@gmail.com>
2024-07-08 11:04:31 +0200

    correction

--- ktLightbox.js
+++ ktLightbox.js
@@ -1,20 +1,23 @@
 (function () {
 	document.addEventListener('DOMContentLoaded', () => {
-		if (
-			window.location.pathname === '/komunikaty-turystyczne' &&
-			imgs.length > 0
-		) {
-			let imgs = document.querySelectorAll('.information__content img');
-			imgs.forEach(function (img) {
-				const url = img.src;
-				const alt = img.alt;
-				var template = `<a href=${url} class="glightbox">
-	<img src=${url} alt="${alt}" height="84">
-	</a>`;
-				img.parentElement.insertAdjacentHTML('beforeend', template);
-				img.remove();
-			});
-			GLightbox();
+		if (window.location.pathname === '/komunikaty-turystyczne') {
+			var imgs = document.querySelectorAll('.information__content img');
+			if (imgs.length > 0) {
+				imgs.forEach(function (img) {
+					debugger;
+					let url = img.src;
+					let cl = img.className;
+					let alt = img.alt;
+					console.log(alt);
+					var template = `<a href=${url} class="glightbox">
+			<img class=${cl} src=${url} alt="${alt}" height="84">
+			</a>`;
+					console.log(template);
+					img.parentElement.insertAdjacentHTML('beforeend', template);
+					img.remove();
+				});
+				GLightbox();
+			}
 		}
 	});
 })();

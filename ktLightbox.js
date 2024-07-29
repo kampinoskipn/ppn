3b8a538542dfe06cdd1db2658f7a3a98aae5233d
Piotr Fidler <fidler.piotr@gmail.com>
2024-07-29 09:18:26 +0200

    remove consol.log

--- ktLightbox.js
+++ ktLightbox.js
@@ -4,15 +4,12 @@
 			var imgs = document.querySelectorAll('.information__content img');
 			if (imgs.length > 0) {
 				imgs.forEach(function (img) {
-					debugger;
 					let url = img.src;
 					let cl = img.className;
 					let alt = img.alt;
-					console.log(alt);
 					var template = `<a href=${url} class="glightbox">
 			<img class=${cl} src=${url} alt="${alt}" height="84">
 			</a>`;
-					console.log(template);
 					img.parentElement.insertAdjacentHTML('beforeend', template);
 					img.remove();
 				});
